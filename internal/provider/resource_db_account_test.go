ac173a361ee727bcc4f4a6542b68cd7ae5d22c12
Rama Venkata Loya <ramavenkata.loya@cyberark.com>
2024-09-06 19:24:24 +0530

    added the acceptance test cases

--- internal/provider/resource_db_account_test.go
+++ internal/provider/resource_db_account_test.go
@@ -2,11 +2,13 @@ package provider_test
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/aharriscybr/terraform-provider-cybr-sh/internal/provider"
-
 	fwresource "github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestDBAccountResourceSchema(t *testing.T) {
@@ -29,4 +31,60 @@ func TestDBAccountResourceSchema(t *testing.T) {
 	if diagnostics.HasError() {
 		t.Fatalf("Schema validation diagnostics: %+v", diagnostics)
 	}
+}
+
+func TestAccDBAccountResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + testAccDBAccountCreateData(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "name", os.Getenv("TF_DB_NAME")),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "address", "1.2.3.4"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "username", os.Getenv("TF_DB_USERNAME")),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "platform", "MySQL"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "safe", "Testsafe"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "secret", os.Getenv("TF_DB_SECRET")),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "secret_name_in_secret_store", "user"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "sm_manage", "false"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "sm_manage_reason", "No CPM Associated with Safe"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "db_port", "8432"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "db_dsn", "dsn"),
+					resource.TestCheckResourceAttr("cybr-sh_db_account.test", "dbname", "dbo.services"),
+					resource.TestCheckResourceAttrSet("cybr-sh_db_account.test", "id"),
+					resource.TestCheckResourceAttrSet("cybr-sh_db_account.test", "last_updated"),
+				),
+			},
+			{
+				Config: providerConfig + `
+				  removed {
+					from = cybr-sh_db_account.test
+					lifecycle {
+						destroy = false
+					}
+				
+				}`,
+			},
+		},
+	})
+}
+
+func testAccDBAccountCreateData() string {
+	return fmt.Sprintf(`
+	resource "cybr-sh_db_account" "test" {
+		name                        = %[1]q
+		address                     = "1.2.3.4"
+		username                    = %[2]q
+		platform                    = "MySQL"
+		safe                        = "Testsafe"
+		secret                      = %[3]q
+		secret_name_in_secret_store = "user"
+		sm_manage                   = false
+		sm_manage_reason            = "No CPM Associated with Safe"
+		db_port                     = "8432"
+		db_dsn                      = "dsn"
+		dbname                      = "dbo.services"
+}
+	`, os.Getenv("TF_DB_NAME"), os.Getenv("TF_DB_USERNAME"), os.Getenv("TF_DB_SECRET"))
 }
