07d9e745627925b125ad476e3349419112f803e8
Samir Shetty <samir.bhatt@cyberark.com>
2025-03-25 12:55:15 -0700

    Ensure test resources are cleaned up

--- bin/utils.sh
+++ bin/utils.sh
@@ -18,70 +18,89 @@ function generateToken() {
   echo "$token"
 }
 
-# Function to disable and delete policy
-function disableAndDeletePolicy() {
+function getPolicies(){
+  local domain=$1 token=$2
+
+  curl -s -H "Authorization: Bearer $token" \
+    -X GET "https://$domain.secretshub.cyberark.cloud/api/policies" \
+    -H 'Accept: application/json' | jq -r '.policies'
+}
+
+function deletePolicy() {
   local domain=$1 token=$2 policy_id=$3
   local status
-  
+
   # Disable the policy
   status=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer $token" \
     -X PUT "https://$domain.secretshub.cyberark.cloud/api/policies/$policy_id/state" \
     -H 'Accept: application/json' \
     -H 'Content-Type: application/json' \
     -d '{ "action": "disable" }')
-  
-  if [ "$status" -eq 200 ]; then
-    # Delete the policy
-    status=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer $token" \
-      -X DELETE "https://$domain.secretshub.cyberark.cloud/api/policies/$policy_id" \
-      -H 'Accept: application/json')
 
-    [ "$status" -eq 200 ] && echo "Successfully deleted the sync policy: $policy_id" || echo "Failed to delete the sync policy($policy_id): $status"
-  else
-    echo "Failed to disable the sync policy: $policy_id"
+  if [ "$status" -ne 200 ]; then
+    echo "Failed to disable policy: $policy_id"
+    echo "Trying to delete the policy anyway..."
   fi
+
+  echo "Deleting policy: $policy_id"
+  curl -s -H "Authorization: Bearer $token" \
+    -X DELETE "https://$domain.secretshub.cyberark.cloud/api/policies/$policy_id" \
+    -H 'Accept: application/json'
+  echo " - Policy deleted: $policy_id"
 }
 
-# Function to delete secret store
-function deleteSecretStore() {
-  local domain=$1 token=$2 store_id=$3
-  local status
+function getSecretStores(){
+  local domain=$1 token=$2
 
-  # Delete the secret store
-  status=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer $token" \
+  # Exclude default PAM_PCLOUD secret store
+  curl -s -H "Authorization: Bearer $token" \
+    -X GET "https://$domain.secretshub.cyberark.cloud/api/secret-stores" \
+    -H 'Accept: application/json' | jq -r '.secretStores | map(select(.type != "PAM_PCLOUD"))'
+}
+
+function deleteSecretStore(){
+  local domain=$1 token=$2 store_id=$3
+
+  echo "Deleting secret store: $store_id"
+  curl -s -H "Authorization: Bearer $token" \
     -X DELETE "https://$domain.secretshub.cyberark.cloud/api/secret-stores/$store_id" \
-    -H 'Accept: */*')
-
-  if [ "$status" -eq 204 ]; then
-    echo "Successfully deleted the SecretStore: $store_id"
-  else
-    echo "Failed to delete the SecretStore($store_id): $status"
-  fi
+    -H 'Accept: application/json'
+  echo " - Secret store deleted: $store_id"
 }
 
-# Function to get policy ID from terraform.tfstate
-function getPolicyID() {
-  local dir=$1
-  if [ -d "$dir" ]; then
-    jq -r '.resources[] | select(.type == "cyberark_sync_policy") | .instances[0].attributes.id' \
-      < "$dir/terraform.tfstate"
-  fi
+function getAccounts(){
+  local domain=$1 token=$2
+
+  # Exclude default accounts
+  curl -s -H "Authorization: Bearer $token" \
+    -X GET "https://$domain.privilegecloud.cyberark.cloud/passwordvault/api/accounts" \
+    -H 'Content-Type: application/json'| jq -r '.value | map(select(.platformId != ""))'
 }
 
-# Function to get store ID From API
-function getStoreID(){
-   local domain=$1 token=$2
-    curl -s -H "Authorization: Bearer $token" \
-    -X GET "https://$domain.secretshub.cyberark.cloud/api/secret-stores?behavior=SECRETS_TARGET&filter=type%20EQ%20AWS_ASM" \
-    -H 'Accept: application/json' | jq -r '.secretStores[] | select(.name == "aws_store") | .id'
+function deleteAccount(){
+  local domain=$1 token=$2 account_id=$3
+
+  echo "Deleting account: $account_id"
+  curl -s -H "Authorization: Bearer $token" \
+    -X DELETE "https://$domain.privilegecloud.cyberark.cloud/passwordvault/api/accounts/$account_id" \
+    -H 'Content-Type: application/json'
+  echo " - Account deleted: $account_id"
 }
 
-# Function to fetch Policy ID from API
-function fetchPolicyIDFromApi(){
-  local domain=$1 token=$2 store_id=$3
-   if [ -n "$store_id" ]; then
-     curl -s -H "Authorization: Bearer $token" \
-      -X GET "https://$domain.secretshub.cyberark.cloud/api/policies?filter=target.id%20EQ%20$store_id" \
-      -H 'Accept: application/json' | jq -r '.policies[].id' 
-   fi
+function getSafes(){
+  local domain=$1 token=$2
+
+  curl -s -H "Authorization: Bearer $token" \
+    -X GET "https://$domain.privilegecloud.cyberark.cloud/passwordvault/api/safes" \
+    -H 'Content-Type: application/json' | jq -r '.value | map(select(.creator.name != "Administrator"))'
+}
+
+function deleteSafe(){
+  local domain=$1 token=$2 safe_name=$3
+
+  echo "Deleting safe: $safe_name"
+  curl -s -H "Authorization: Bearer $token" \
+    -X DELETE "https://$domain.privilegecloud.cyberark.cloud/passwordvault/api/safes/$safe_name" \
+    -H 'Content-Type: application/json'
+  echo " - Safe deleted: $safe_name"
 }
